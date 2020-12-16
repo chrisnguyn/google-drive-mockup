1be296e4cdfe354c44d89e26462bebdbd30d92f0
Christopher Nguyen <chrisngyn99@gmail.com>
2020-12-16 02:10:12 -0500

    base html files complete

--- src/main.py
+++ src/main.py
@@ -1,12 +1,10 @@
-from flask import Flask
-
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('base.html')
 
 
 if __name__ == '__main__':
